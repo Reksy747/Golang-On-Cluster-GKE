9342530f90e215aa12affa84d9859cbc409d9fc2
reksy <petrus.chanel@ti.ukdw.ac.id>
2023-01-29 22:30:13 +0700

    first init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Reksy747/simple_website_go
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// The new router function creates the router and
+// returns it to us. We can now use this function
+// to instantiate and test the router outside of the main function
+func newRouter() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handler).Methods("GET")
+
+	// Declare the static file directory and point it to the
+	// directory we just made
+	staticFileDirectory := http.Dir("./portofolio/")
+	// Declare the handler, that routes requests to their respective filename.
+	// The fileserver is wrapped in the `stripPrefix` method, because we want to
+	// remove the "/portofolio/" prefix when looking for files.
+	// For example, if we type "/portofolio/index.html" in our browser, the file server
+	// will look for only "index.html" inside the directory declared above.
+	// If we did not strip the prefix, the file server would look for
+	// "./portofolio/portofolio/index.html", and yield an error
+	staticFileHandler := http.StripPrefix("/portofolio/", http.FileServer(staticFileDirectory))
+	// The "PathPrefix" method acts as a matcher, and matches all routes starting
+	// with "/portofolio/", instead of the absolute route itself
+	r.PathPrefix("/portofolio/").Handler(staticFileHandler).Methods("GET")
+	return r
+}
+
+func main() {
+	// The router is now formed by calling the `newRouter` constructor function
+	// that we defined above. The rest of the code stays the same
+	r := newRouter()
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
